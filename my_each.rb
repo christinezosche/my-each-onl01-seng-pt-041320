9e0563e258963cab0a1fc08f384a46c7006e2bc8
Christine Zosche <christine.zosche@gmail.com>
2020-04-23 16:27:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,11 @@
 collection = [1, 2, 3, 4]
-def my_each(collection)
-  collection.each do |i|
-    puts i
+def my_each(array)
+  i = 0
+  while i <= array.length
+    yield[i]
+    i += 1
   end
-
   collection
-  
 end
+
+my_each(collection){|i| puts i}
